b57417c51818020b7d1a2647a11b4ba14ff931df
Evonne Truong <evonne.truong@sjsu.edu>
2021-04-10 18:16:14 -0700

    jesus help me

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
 function setup() {
   createCanvas(500, 500);
+  background(220);
+  strokeWeight(5);
+
 }
 
 function draw() {
-  background(220);
+// console.log(mouseIsPressed);
+
+if (mouseIsPressed){
+  stroke(map(mouseX, 0, 600, 0, 255, true));
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+}
+}
+
+function keyTyped(){
+  // console.log(`key s is being typed`)
+  console.log(`key ${key} is being typed`)
+  if (key === "s"){
+    //save this images
+    saveCanvas('fileName', 'png')
+  }
+
+  return false;
 }
