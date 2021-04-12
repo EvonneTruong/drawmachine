7a5e1559ff429197ba22728a8b5015cbbea714de
Evonne Truong <evonne.truong@sjsu.edu>
2021-04-11 17:08:57 -0700

    bsics

--- sketch.js
+++ sketch.js
@@ -1,31 +1,41 @@
-let draw = []
+let array = [];
 
 function setup() {
   createCanvas(500, 500);
   background(220);
   strokeWeight(5);
+  // stroke(map(mouseX, 0, 600, 0, 255, true));
+  noFill();
 
 }
 
 function draw() {
-// console.log(mouseIsPressed);
 
-if (mouseIsPressed){
-  stroke(map(mouseX, 0, 600, 0, 255, true));
-  line(mouseX, mouseY, pmouseX, pmouseY);
-  // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-  array.push([mouseX, mouseY]);
-}
+  if (mouseIsPressed) {
+    // stroke(map(mouseX, 0, 600, 0, 255, true));
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+background(0);
+    array.push([mouseX, mouseY]);
+  }
 }
 
-function keyTyped(){
-  // console.log(`key s is being typed`)
-  console.log(`key ${key} is being typed`)
-  if (key === "s"){
+function keyTyped() {
+
+  if (key === "s") {
     //save this images
-    saveCanvas('fileName', 'png')
-  } else if (key === "d"){
-    //display images
+    saveCanvas('fileName', 'png');
+  } else if (key === "d") {
+    //displa;y images
+    background(220);
+
+beginShape();
+for(let i = 0; i < array.length; i++){
+  console.log(i);
+  // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+  curveVertex(array[i][0], array[i][1])
+}
+endShape();
   }
 
   return false;
